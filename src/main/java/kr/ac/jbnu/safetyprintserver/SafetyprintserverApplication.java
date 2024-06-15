29a26f2762fb3e308049da9518a82642973a586a
NamYSu <namsu2670@gmail.com>
2024-06-15 19:08:39 +0900

    initial commit
    Signed-off-by: NamYSu <namsu2670@gmail.com>

---
+++ src/main/java/kr/ac/jbnu/safetyprintserver/SafetyprintserverApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.safetyprintserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyprintserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyprintserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/jbnu/safetyprintserver/SafetyprintserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jbnu.safetyprintserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyprintserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
